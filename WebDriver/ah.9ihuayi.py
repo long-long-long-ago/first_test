45f75b12aa1aaa0134e605d1f6823786c8a76c5c
looper <1963270898@qq.com>
2023-07-19 16:57:23 +0800

    。。

--- WebDriver/ah.9ihuayi.py
+++ WebDriver/ah.9ihuayi.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 from WebDriver import Properties
 from selenium import webdriver
-from selenium.common import NoSuchElementException
+
 from selenium.webdriver.common.by import By
 import ddddocr
 import pymysql
